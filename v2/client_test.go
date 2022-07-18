a215cd10e50b6b0830b092d18536b6a1fa3fa865
Maksim Falaleev <maxfalaleev1@gmail.com>
2022-07-18 17:18:17 +0300

    up public credentials

--- v2/client_test.go
+++ v2/client_test.go
@@ -10,6 +10,17 @@ func createTestClient() Client {
 	wd, _ := os.Getwd()
 	godotenv.Load(fmt.Sprintf("%s/.env.local", wd))
 
+	clientId := os.Getenv("CDEK_CLIENT_ID")
+	clientSecretId := os.Getenv("CDEK_SECRET_ID")
+
+	// public cdek test credentials
+	if clientId == "" {
+		clientId = "EMscd6r9JnFiQ3bLoyjJY6eM78JrJceI"
+	}
+	if clientSecretId == "" {
+		clientSecretId = "PjLZkKBHEiLK3YsjtNrt3TGNG0ahs3kG"
+	}
+
 	return NewClient(&Options{
 		Endpoint: EndpointTest,
 		Credentials: &Credentials{
